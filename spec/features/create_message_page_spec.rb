cf7defa091074ab60cb01c9b205ba623bfe18f97
Guy Halperin & J.J. Idt <student@epicodus.com>
2014-09-09 17:05:20 -0700

    add integration test for successful message

--- spec/features/create_message_page_spec.rb
+++ spec/features/create_message_page_spec.rb
@@ -15,4 +15,13 @@ describe 'creating a new message' do
     click_link "send-message"
     expect(page).to have_content "From"
   end
+
+  it 'successfully sends a message' do
+    click_link "send-message"
+    fill_in 'message_to', :with => '3145834894'
+    fill_in 'message_from', :with => '3144925314'
+    fill_in 'message_body', :with => "tester"
+    click_button ''
+    expect(page).to have_content "Sent"
+  end
 end
